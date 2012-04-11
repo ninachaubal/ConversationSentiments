298c5face76200419c3cbb725a8806a3dfcf6459
Niraj Chaubal <nirajchaubal@gmail.com>
2012-04-11 23:32:37 +0000

    added files

--- server.js
+++ server.js
@@ -1,26 +1,7 @@
-var app = require('http').createServer(handler)
-, io = require('socket.io').listen(app)
-, fs = require('fs')
+var http = require('http');
 
-app.listen(process.env.PORT || 8001);
-
-function handler (req, res) {
-fs.readFile('index.html',
-function (err, data) {
-if (err) {
-res.writeHead(500);
-return res.end('Error loading index.html');
-}
-
-res.writeHead(200, {'Content-Type': 'text/html', "Content-Length": data.length});
-res.end(data);
+var server = http.createServer(function(req,res){
+    res.writeHead(200);
+    res.end("hello world");
 });
-}
-
-io.sockets.on('connection', function (socket) {
-// echo the message
-socket.on('message', function (data) {
-console.info(data);
-socket.send("[ECHO] "+data);
-});
-});
+server.listen(8080)
