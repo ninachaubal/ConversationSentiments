05edcd2c225d57c8555c9a799d73cf0c420b073d
Niraj Chaubal <nirajchaubal@gmail.com>
2012-09-19 18:34:21 -0400

    changed server to use sentimental

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-
 //colors
 var col = require('./colors');
+var colors = col.colors();
 //Sentimental
-var sentimental = require('Sentimental');
+var analyze = require('sentimental').analyze;
 //create a server
 var express = require('express');
 var app = express();
-//array of users
+
+//array users
 var users = [];
+//id to be issued to next user 
+var nextId = 0;
 
 //chat stream
 var chat = [];
@@ -27,10 +30,13 @@ app.use('/test', express.static(__dirname + '/test'));
 /*
 GET /users
 returns the current list of users 
-color is the base color of this user
 example output - 
 [{"name":"user1","color":"#11AFBA"},
  {"name":"user2","color":"#A82A2A"}, ...]
+
+color is the base color of the user
+index in the array is the id
+users who have left the conversation will be undefined
 */
 app.get('/users', function(req, res){
     res.json(users,200);
@@ -43,7 +49,7 @@ last = the last index that was recieved from previous calls.
 if this is the first call, call with negative last
 returns the chat stream
 example output - 
-[{"name":"user1", "color":"#11AFBA","text":"Hi","index":"10",
+[{"name":"user1", "id":1, "color":"#11AFBA","text":"Hi","index":"10",
   "time":"1256953732", "sentiment":{...}}, ...]
 
 the sentiment object is as returned by Sentimental.analyze()
@@ -63,70 +69,58 @@ app.get('/stream', function(req, res){
 /*
 POST /user
 params:
-name = the user name of the user to add 
-theme = the color theme for the user 
-pos = the position of the user (see position in User.join)
+name = the user name of the user to add
+returns a unique id for this user;
+example output
+{"id":1}
 */
 app.post('/user', function(req, res){
-    //TODO: change this
     var name = req.param('name');
-    var theme = req.param('theme');
-    var pos = req.param('pos');
-    if(pos !== undefined && pos >= 0 && pos <= 3 &&
-       theme !== undefined && theme > 0 && theme <= 4 &&
-       name !== undefined && name.length > 0){
-        inUse = true;
-        table[pos].join(theme, name, pos);
-        res.send('',200);
+    if(name !== undefined && name.length > 0){
+        var color = colors.getColor();
+        users[nextId] = {"name": name, "color": color};
+        var ret = {"id": nextId};
+        nextId++;
+        res.json(ret, 200);
     }
+    res.send('',400);
 });
 
 /*
 DELETE /user
 params:
-pos = position of the user
+id = id of the user
 */
 app.del('/user',function(req,res){
-    // TODO: change this
-    var pos = req.param('pos');
-    if(pos != undefined && pos >= 0 && pos <= 3){
-        table[pos].leave();
-        inUse = false;
-        for(var i in table){
-            if(table[i].isConversing){
-                inUse = true;
-                break;
-            }
-        }
-        if(!inUse){
-            reset();
-        }
+    var id = req.param('id');
+    if(id != undefined && id >= 0 && id < nextId){
+        users[id] = undefined;
+        res.send('',200);
     }
-    res.send('',200);
+    res.send('',400);
 });
 
 /*
 POST /chat
 params:
-pos = the position of the user
+id = the id of the user
 text = the stuff the user said
 amp = amplitude (0-8. defaults to 4)
 */
 app.post('/chat', function(req, res){
-    //TODO: change this
-    var pos = req.param('pos');
+    var id = req.param('id');
     var text = req.param('text');
     var amp = req.param('amp');
-    if(pos !== undefined && pos >= 0 && pos <=3 &&
+    if(id !== undefined && id >= 0 && id < nextId && users[id] !== undefined &&
        text !== undefined && text.length > 0){
         if (amp === undefined) {
             amp = 4;
         }
-        addChatLine(text,pos, amp);
+        addChatLine(text,id, amp);
+        res.send('',200);
     }
-    res.send('',200);
+    res.send('',400);
 });
-
 
 /*
 GET /reset
@@ -141,24 +135,25 @@ app.get('/reset', function(req, res){
 adds text to the chat stream
 returns the index where the last chat line was added
 */
-function addChatLine(text, pos, amp){
-    //TODO: change this
+function addChatLine(text, id, amp){
     if(chat.length == chatMax){
         chat.pop();
     }
+    var sentiment = analyze(text);
     var obj = {
-        "pos" : pos,
-        "color" : table[pos].color,
+        "id" : id,
         "text" : text,
         "index" : chatIndex,
-        "name" : table[pos].name,
-        "time" : String(Math.round(new Date().getTime() / 1000))
+        "name" : users[id].name,
+        "time" : String(Math.round(new Date().getTime() / 1000)),
+        "sentiment": sentiment,
+        "color" : getSentimentColor(users[id].color, sentiment.score)
     };
 }
 
 /*
 takes a #rrbbgg color string, adjusts value in hsv space as per the score
-which is in a [-1,1] range
+which is in a [-5,5] range
 and returns another color string
 */
 function getSentimentColor(color, score){
@@ -170,6 +165,7 @@ function getSentimentColor(color, score){
     var b = parseInt(color.substring(5,7),16);
     var hsv = col.rgbToHsv(r,g,b);
     //change the value (hsv[2])
+    score /= 5; //score is now in [-1,1] range
     score += 1; //score is now in [0,2] range
     hsv[2] = Math.round(40 + score*30);
     var rgb = col.hsvToRgb(hsv[0],hsv[1],hsv[2]);
