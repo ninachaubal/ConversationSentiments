139c43593a120b3aded2882912ef28faec6038e4
Niraj Chaubal <nirajchaubal@gmail.com>
2012-09-19 18:00:17 -0400

    trimmed server

--- server.js
+++ server.js
@@ -1,32 +1,18 @@
 
-//user
-var usr = require('./user');
 //colors
 var col = require('./colors');
-//http object
-var http = require('http');
+//Sentimental
+var sentimental = require('Sentimental');
 //create a server
 var express = require('express');
-var app = express.createServer();
-//array of users - we support at most 4 users at a time
-var table = [new usr.User(),new usr.User(),new usr.User(),new usr.User()];
+var app = express();
+//array of users
+var users = [];
+
 //chat stream
 var chat = [];
 var chatIndex = 0;
-var chatMax = 1000;
-
-//sentiment output stream
-var sentiment = [];
-var sentimentIndex = 0;
-var sentimentMax = 5000;
-
-//circles
-var circles = [];
-
-var inUse = false;
-
-//sentiwordnet
-var swn = false;
+var chatMax = 5000;
 
 /*
 deliver index.html
@@ -39,24 +25,15 @@ deliver test.html
 app.use('/test', express.static(__dirname + '/test'));
 
 /*
-GET /table 
-returns the current list of users on the table
+GET /users
+returns the current list of users 
+color is the base color of this user
 example output - 
-[{"name":"user1","color":"#11AFBA","position":"0","theme":1},
- {"name":"user2","color":"#A82A2A","position":"1","theme":3}, ...]
+[{"name":"user1","color":"#11AFBA"},
+ {"name":"user2","color":"#A82A2A"}, ...]
 */
-app.get('/table', function(req, res){
-    var arr = [];
-    for(var i in table){
-        var obj = table[i].getData();
-        if(obj !== null){
-            if(obj.position < 0){
-                obj.position = i;
-            }
-            arr.push(obj);
-        } 
-    }
-    res.json(arr,200);
+app.get('/users', function(req, res){
+    res.json(users,200);
 });
 
 /*
@@ -66,10 +43,10 @@ last = the last index that was recieved from previous calls.
 if this is the first call, call with negative last
 returns the chat stream
 example output - 
-[{"name":"user1", "color":"#11AFBA","text":"Hi","index":"10","pos":"1","time":"1256953732"},
-{"name":"user2", "color":"#A82A2A","text":"ssup?","index":"11","pos":"4","time":"1256958752"},
-{"name":"user1", "color":"#11AFBA","text":"nm","index":"12","pos":"1","time":"1257958442"}]
+[{"name":"user1", "color":"#11AFBA","text":"Hi","index":"10",
+  "time":"1256953732", "sentiment":{...}}, ...]
 
+the sentiment object is as returned by Sentimental.analyze()
 the color is adjusted as per the average sentiment
 */
 app.get('/stream', function(req, res){
@@ -84,28 +61,6 @@ app.get('/stream', function(req, res){
 });
 
 /*
-GET /sentiments
-params:
-last = the last index that was recieved from previous calls.
-if this is the first call, call with negative last
-returns the sentiment stream
-example output -
-[{"pos":"4","color":"#0D3233", "text":"keyword","index"="4"},{...}]
-
-the color is adjusted as per sentiment score for each keyword
-*/
-app.get('/sentiments', function(req, res){
-    var last = req.param('last');
-    var arr = [];
-    for(var i in sentiment){
-        if(sentiment[i].index > last){
-            arr.push(sentiment[i]);
-        }
-    }
-    res.json(arr,200);
-});
-
-/*
 POST /user
 params:
 name = the user name of the user to add 
@@ -113,10 +68,11 @@ theme = the color theme for the user
 pos = the position of the user (see position in User.join)
 */
 app.post('/user', function(req, res){
+    //TODO: change this
     var name = req.param('name');
     var theme = req.param('theme');
     var pos = req.param('pos');
-    if(pos != undefined && pos >= 0 && pos <= 3 &&
+    if(pos !== undefined && pos >= 0 && pos <= 3 &&
        theme !== undefined && theme > 0 && theme <= 4 &&
        name !== undefined && name.length > 0){
         inUse = true;
@@ -131,6 +87,7 @@ params:
 pos = position of the user
 */
 app.del('/user',function(req,res){
+    // TODO: change this
     var pos = req.param('pos');
     if(pos != undefined && pos >= 0 && pos <= 3){
         table[pos].leave();
@@ -153,38 +110,23 @@ POST /chat
 params:
 pos = the position of the user
 text = the stuff the user said
+amp = amplitude (0-8. defaults to 4)
 */
 app.post('/chat', function(req, res){
+    //TODO: change this
     var pos = req.param('pos');
     var text = req.param('text');
+    var amp = req.param('amp');
     if(pos !== undefined && pos >= 0 && pos <=3 &&
        text !== undefined && text.length > 0){
-        addChatLine(text,pos);
+        if (amp === undefined) {
+            amp = 4;
+        }
+        addChatLine(text,pos, amp);
     }
     res.send('',200);
 });
 
-/*
-GET postcircles 
-This is pretty much a terrible idea
-params:
-arr = [{'x':3,'y':4,'rad':5,'ang':'-1','chr':'a',col:'#2323ff'}, ...]
-*/
-app.get('/postcircles', function(req,res){
-    var arr = req.param('arr');
-    if(arr !== undefined){
-        console.log(arr);
-        circles = JSON.parse(arr);
-        res.send('',200);
-    }
-});
-
-/*
-GET /circles
-*/
-app.get('/circles', function(req, res){
-    res.json(circles,200);
-});
 
 /*
 GET /reset
@@ -196,58 +138,11 @@ app.get('/reset', function(req, res){
 });
 
 /*
-POST /swn
-param:
-val : true or false
-false = use AlchemyAPI
-true = use SentiWordNet
-*/
-app.post('/swn',function(req,res){
-    var val = req.param('val');
-    if(val !== undefined){
-        swn = (val == 'true');
-    }
-});
-
-/*
-adds keywords to the sentiment stream
-also adds chatObj to the chat stream after adjusting its color
-*/
-function addSentiment(text, pos, chatObj){
-    if(sentiment.length == sentimentMax){
-        sentiment.pop();
-    }
-    getSentiments(text, function(keywords){
-        var avg = 0;
-        for (var i in keywords){
-            var color = getSentimentColor(col.theme.getColor(table[pos].theme),
-                                              parseFloat(keywords[i].sentiment));
-            var obj = {
-                "pos" : pos,
-                "color" : color,
-                "text" : keywords[i].text,
-                "index" : sentimentIndex
-            };
-            sentimentIndex++;
-            sentiment.push(obj);
-            avg += parseFloat(keywords[i].sentiment);
-        }
-        if(keywords.length != 0){
-            avg = avg / keywords.length;
-            //adjust color in chat
-            chatObj.color = getSentimentColor(chatObj.color,avg);
-        }
-        //add the chat object with the sentiment adjusted color
-        chatIndex++;
-        chat.push(chatObj);
-    });
-}
-
-/*
 adds text to the chat stream
 returns the index where the last chat line was added
 */
-function addChatLine(text, pos){
+function addChatLine(text, pos, amp){
+    //TODO: change this
     if(chat.length == chatMax){
         chat.pop();
     }
@@ -259,80 +154,6 @@ function addChatLine(text, pos){
         "name" : table[pos].name,
         "time" : String(Math.round(new Date().getTime() / 1000))
     };
-    addSentiment(text, pos, obj);
-}
-
-/*
-extracts sentiment scores from the text
-uses AlchemyAPI by default. 
-uses SentiWordNet if swn is true
-*/
-function getSentiments(text,callback){
-    if(swn){
-        getSentimentsFromSWN(text,callback);
-    }
-    else{
-        getSentimentsFromAlchemy(text,callback);
-    }
-}
-
-/*
-extracts sentiment score using AlchemyAPI
-*/
-function getSentimentsFromAlchemy(text, callback){
-    var apikey = '1e9868483244e57f2ddb61670c0b361ed2a2f204';
-    var alchemy = {
-        host: 'access.alchemyapi.com',
-        path: '/calls/text/TextGetRankedKeywords?apikey='+ apikey +
-              '&outputMode=json&maxRetrieve=5&sentiment=1&text=' 
-              + encodeURIComponent(text)
-    };
-
-    http.get(alchemy, function(res){
-        var data = "";
-        res.on('data', function(chunk){
-            data+=chunk;
-        });
-        res.on('end', function(){
-            var dataobj = JSON.parse(data);
-            var keywords = [];
-            for(var i in dataobj.keywords){
-                var t = dataobj.keywords[i].text;
-                var s = 0;
-                if(dataobj.keywords[i].sentiment.type != 'neutral'){
-                    s = dataobj.keywords[i].sentiment.score;
-                }
-                keywords.push({
-                    'text': t,
-                    'sentiment' : s
-                });
-            }
-            callback(keywords);
-        });
-    });
-}
-
-/*
-extracts sentiment score using SentiWordNet
-this function does absolutely nothing to select important keywords
-which may result in it generating a lot of keyword
-*/
-function getSentimentsFromSWN(text, callback){    
-    var sentiwordnet = {
-        host: 'www.tweetsichord.com',
-        path: '/sentiwordnet.php?q=' + encodeURIComponent(text)
-    };
-
-    http.get(sentiwordnet, function(res){
-        var data = "";
-        res.on('data', function(chunk){
-            data+=chunk;
-        });
-        res.on('end', function(){
-            var dataObj = JSON.parse(data);
-            callback(dataObj);
-        });
-    });
 }
 
 /*
@@ -368,19 +189,10 @@ reset the conversation
 */
 function reset(){
     //remove all users
-    for(var i in table){
-        table[i].leave();
-    }
+    users = [];
     //reset chat
     chat = [];
     chatIndex = 0;
-    //reset  sentiment
-    sentiment = [];
-    sentimentIndex = 0;
-    //reset circles
-    circles = [];
-    
-    inUse = false;
 }
 
 app.listen(process.env.PORT);
