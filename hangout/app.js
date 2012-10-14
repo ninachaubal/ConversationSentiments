e1c6ac4408af8abafbde76c1075adc2300048df4
Niraj Chaubal <nirajchaubal@gmail.com>
2012-10-14 16:35:03 -0400

    changed file structure

--- hangout/app.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    var str = "hello, ";
-    var parti = gapi.hangout.getEnabledParticipants();
-    for (var i in parti) {
-        str += parti[i].person.displayName;
-    }
-    $('#name').html(str);
-});
