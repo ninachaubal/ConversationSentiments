cb3d8e318d5a5455d173b164d576769354d0add6
Niraj Chaubal <nirajchaubal@gmail.com>
2012-10-16 02:15:46 -0400

    changed sketch

--- static/sketch.js
+++ static/sketch.js
@@ -1,7 +1,6 @@
-function sketchProc(processing) {
+function sketch(processing) {
   processing.setup = function() {};
   processing.draw = function() {};
 }
 
-var canvas = document.getElementById("canvas1");
-var p = new Processing(canvas, sketchProc);
+new Processing("canvas1", sketch);
