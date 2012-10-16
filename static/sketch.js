8ed54e20e5064b751edc888fdb76354bd10ecad5
Niraj Chaubal <nirajchaubal@gmail.com>
2012-10-16 02:09:53 -0400

    moved sketch from pde to js

---
+++ static/sketch.js
@@ -0,0 +1,7 @@
+function sketchProc(processing) {
+  processing.setup = function() {};
+  processing.draw = function() {};'
+}
+
+var canvas = document.getElementById("canvas1");
+var p = new Processing(canvas, sketchProc);
