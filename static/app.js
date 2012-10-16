090e306f0a361e9483b46705a3cbd8e76dbc9100
Niraj Chaubal <nirajchaubal@gmail.com>
2012-10-16 04:37:16 -0400

    adding video canvas

--- static/app.js
+++ static/app.js
@@ -1,31 +1,82 @@
-var type = 'spiral';
-var dataBuffer = [];
-var participants = {};
+var colors = function() {
+  this.hues = [{'id': 'dummy', 'h': 360}];
+  this.assigned0 = false;
+};
 
-var serverURL = 'https://conversationsentiments.herokuapp.com';
+/*
+Returns an unused hue as distinct as possible from colors already being used
+*/
+colors.prototype.getColor = function(id) {
+  if (!this.assigned0) {
+    this.hues.unshift({'id': id, 'h': 0});
+    this.assigned0 = true;
+    return 0;
+  } else {
+    var lowerPos = -1;
+    var upperPos = -1;
+    var longestRange = 0;
+    for (var i in this.hues) {
+      if (this.hues[i+1] !== undefined) {
+        var range = this.hues[i+1].h - this.hues[i].h;
+        if (range > longestRange) {
+          longestRange = range;
+          lowerPos = i;
+          upperPos = i + 1;
+        }
+      }
+    }
+    if (this.hues[lowerPos] !== undefined &&
+        this.hues[upperPos] !== undefined) {
+      var h = Math.round((this.hues[lowerPos].h + this.hues[upperPos].h) / 2);
+      this.hues.splice(lowerPos + 1,0,{'id': id, 'h': h});
+      return h
+    }
+    return undefined;
+  }
+};
+
+/*
+Frees up hues associated with the given user id
+*/
+colors.prototype.remove = function(id) {
+  for (var i in this.hues) {
+    if (this.hues[i].id == id) {
+      if (this.hues[i].h === 0) {
+        this.assigned0 = false;
+      }
+      this.hues.splice(i,1);
+      break;
+    }
+  }
+};
+
+//============================================================================//
+
+var col = colors();
+var hueMap = {};
+var videoCanvas;
+var videoFeed;
 
 gapi.hangout.onApiReady.add(function(eventObj) {
-  /*do something*/
-});
-
-$(document).ready(function(){
-  $('#viztype').change(function(){
-    type = $('#viztype').val();
-  });
-});
-
-/*gets updates from server*/
-function update() {
-  $.ajax({
-    url: serverURL + '/data',
-    success: function(data) {
-      dataBuffer = data;
+  gapi.hangout.onParticipantsAdded.add(function(e){
+    for (i in e.addedParticipants) {
+      hueMap[id] = col.getColor(e.addedParticipants[i].id);
     }
   });
-  $.ajax({
-    url: serverURL + '/users',
-    success: function(data) {
-      participants = data;
-    }
+  gapi.hangout.onParticipantsRemoved.add(function(e){
+    //TODO
   });
-}
+  gapi.hangout.onTopicChanged.add(function(e){
+    //TODO
+  });
+
+  videoCanvas = gapi.hangout.layout.getVideoCanvas();
+  videoFeed = gapi.hangout.layout.getDefaultVideoFeed();
+  videoCanvas.setVideoFeed(videoFeed);
+  videoCanvas.setVisible(true);
+  videoCanvas.setPosition(600);
+});
+
+
+
+
