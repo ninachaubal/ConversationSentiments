a369e4360cc800dbef7ad51eb1350d39aa64fa3d
Niraj Chaubal <nirajchaubal@gmail.com>
2012-11-01 16:45:09 -0500

    wami

--- server.js
+++ server.js
@@ -1,38 +1,33 @@
-//colors
-var colors = require('./colors').colors;
-
-//Sentimental
-var analyze = require('Sentimental').analyze;
 //create a server
 var express = require('express');
 var app = express();
-// test
-var test = require('./test');
+var fs = require('fs');
 
-// Data Buffer
-var dataBuffer = [];
+/*
+use multipart request bodies
+*/
+app.use(express.multipart());
 
-// map of participants
-var participants = {};
+/*
+parse request bodies.
+*/
+app.use(function(req, res, next) {
+  var data = '';
+  req.setEncoding('binary');
+  req.on('data', function(chunk) { 
+    data += chunk;
+  });
+
+  req.on('end', function() {
+    req.body.audio = data;
+    next();
+  });
+});
 
 /*
 mount static content at /
 */
 app.use(express.static(__dirname + '/static'));
-
-/*
-parse request bodies
-*/
-app.use(express.bodyParser());
-
-/*
-POST /test
-load test data
-*/
-app.post('/test', function(req, res){
-  test.loadtestdata('testdata-friends.srt', addUser, addData);
-  res.send(200);
-});
 
 /*
 Set headers for cross site requests
@@ -47,128 +42,25 @@ app.post('/*',function(req,res,next){
 });
 
 /*
-POST /users
-params:
-users = [{id: id, name: name} ...]
-where,
-id = participant id as given by the hangouts api
-name = participant's display name
+  POST /audio
+  param: 
+    rec: a unique identifier for this recording
 */
-app.post('/users', function(req, res){
-  var users = req.body.users;
-  var success = false;
-  for (var i in users) {
-    success = success || addUser(users[i].id, users[i].name);
-  }
-  if (success) {
-    res.send(200);
-  } else {
-    res.send(400);
-  }
-});
-
-function addUser(id, name) {
-  if (id !== undefined && name !== undefined && participants[id] === undefined){
-    var h = colors.getColor(id);
-    participants[id] = {'h': h, 's': 100, 'l': 50 ,'name': name};
-    return true;
-  } else {
-    return false;
-  }
-}
-
-
-/*
-  GET /users
-  returns all users currently in the conversation
-*/
-app.get('/users', function(req, res) {
-  res.json(200, participants);
+app.post('/audio', function(req, res){
+  var rec = req.param('rec');
+  var filepath = __dirname + '/tmp/' + Date.now() + '-' + rec + '.wav';
+  var fd = fs.openSync(filepath, 'a');
+  var buff = new Buffer(req.body.audio, 'binary');
+  fs.writeSync(fd, buff, 0, buff.length, 0);
 });
 
 /*
-  DELETE /user
-  params:
-  id = id given by gapi.hangout.getLocalParticipantId()
+  GET /text
+  param: 
+    rec: unique identifier passed to /audio
 */
-app.del('/user', function(req, res) {
-  var id = req.param('id');
-  if (id !== undefined) {
-    colors.remove(id);
-    delete participants[id];
-  } else {
-    res.send(400);
-  }
-});
-
-/*
-  POST /data
-  params:
-  text: the text content of the message
-  id: the id of the participants
-  duration: duration in ms of the message
-  amplitude: amplitude of the recording in a (0,10] range
-  topic: current hangout topic
-*/
-app.post('/data', function(req, res){
-  var id = req.param('id');
-  var text = req.param('text');
-  var duration = parseInt(req.param('duration'),10);
-  var amplitude = parseInt(req.param('amplitude'),10);
-  var topic = req.param('topic');
-  res.send(addData(id, text, duration, amplitude, topic,
-            new Date().getTime()));
-});
-
-function addData(id, text, duration, amplitude, topic, time) {
-  if (id !== undefined && text !== undefined &&
-      duration !== undefined && duration > 0 &&
-      amplitude !== undefined && amplitude > 0 &&
-      amplitude <= 10 && time !== undefined){
-    var analysis = analyze(text);
-    var dataObj = {
-      'id': id,
-      'text': text,
-      'time': time,
-      'duration': duration,
-      'amplitude': amplitude,
-      'topic': topic,
-      'score' : analysis,
-      'h' : participants[id].h,
-      's' : participants[id].s,
-      // converts [-5,5] sentiment score range to [20, 80] lightness values
-      'l' : 20 + ((parseInt(analysis.score,10) + 5) * 6)
-    };
-    dataBuffer.push(dataObj);
-    return 200;
-  } else {
-    return 400;
-  }
-};
-
-/*
-  GET /data
-  returns dataBuffer
-*/
-app.get('/data', function(req, res) {
-  res.json(200, dataBuffer);
-});
-
-/*
-  GET /reset
-  resets the server
-*/
-app.get('/reset', function(req, res){
-  dataBuffer = [];
-  participants = [];
-  colors.reset();
-});
-
-/*
-  GET /time
-*/
-app.get('/time', function(req, res) {
-  res.json(200, {time: new Date().getTime()});
+app.get('/text', function(req, res){
+  var rec = req.param('rec');
 });
 
 if (process.env.PORT !== undefined) {
